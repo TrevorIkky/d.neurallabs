f3f82a69d0abceb4014bbd72f43f9c382c1ade12
Ikky <ikky@pop-os.localdomain>
2021-04-19 14:04:59 +0300

    server.py new changes, load image from url & try catch validation

--- app/server.py
+++ app/server.py
@@ -5,32 +5,40 @@ from array import array
 from keras.models import model_from_json
 from tensorflow import keras
 from keras.preprocessing import image
-
 import numpy as np
 import cv2
-
+import urllib
+ 
 app = Flask(__name__)
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png']
-
-@app.route('/')
-def home():
-    return "Flask running"
-
+ 
 @app.route('/tb-analyzer', methods=['POST'])
 def tb():
-    xray_image = request.files['xray_image']
-    if xray_image.filename != '':
-        xray_image.save(xray_image.filename)
-
-    model = keras.models.load_model('model.h5')
-
-    img  = cv2.imread(xray_image.filename, cv2.IMREAD_GRAYSCALE)
-    img = cv2.resize(img, (300, 300))
-    img_arr = np.array(img).reshape(1,300,300,1)
-
-    response = model.predict(img_arr)
-
-    return jsonify({'Prediction ' : response.tolist()})
-
+    model_successful = False
+    try:
+        if request.form is not None and "url" in request.form:
+            url = request.form["url"]
+ 
+            model = keras.models.load_model('model.h5')
+ 
+            req = urllib.request.urlopen(url)
+            arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+ 
+            img = cv2.imdecode(arr, -1)
+            img = cv2.resize(img, (300, 300))
+            img_arr = np.array(img).reshape(1,300,300,1)
+ 
+            response = model.predict(img_arr)
+            model_successful = True
+        else :
+            response = "URL Error"
+ 
+    except Exception as e:
+        response = str(e)
+ 
+    response = response.tolist() if model_successful else response
+    return jsonify({'Prediction ' : response})
+ 
+ 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
